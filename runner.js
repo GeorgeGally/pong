5968fe3b4eaa53b5008d85288605333a11f91c14
george <robopop@radarboy.com>
2016-11-27 23:31:47 +0800

    added serial

--- runner.js
+++ runner.js
@@ -13,12 +13,16 @@ var SerialPort = require('serialport');
 var port = new SerialPort('COM3', { autoOpen: false });
 
 
-port.open(function (err) {
-  if (err) {
-    return console.log('Error opening port: ', err.message);
-  }
+port.on('open', function() {
+  port.write('main screen turn on', function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+    console.log('message written');
+  });
 });
 
-port.on('data', function (data) {
-  console.log('Data: ' + data);
-}
+// open errors will be emitted as an error event
+port.on('error', function(err) {
+  console.log('Error: ', err.message);
+})
