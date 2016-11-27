cb147c9276c5ac50f6b6ac311e1e638f5fd5972b
george <robopop@radarboy.com>
2016-11-27 23:25:25 +0800

    added serial

--- runner.js
+++ runner.js
@@ -1,3 +1,4 @@
+
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(8080, function(){
@@ -5,15 +6,19 @@ connect().use(serveStatic(__dirname)).listen(8080, function(){
 });
 
 
+// var SerialPort = require('lib/serialport.js');
 var SerialPort = require('serialport');
 // var port = new SerialPort('/dev/tty-usbserial1', { autoOpen: false });
+//var port = new SerialPort('/dev/cu.Bluetooth-Modem', { autoOpen: false });
 var port = new SerialPort('COM3', { autoOpen: false });
+
 
 port.open(function (err) {
   if (err) {
     return console.log('Error opening port: ', err.message);
   }
+});
 
 port.on('data', function (data) {
   console.log('Data: ' + data);
-});
+}
